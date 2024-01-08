f45139090f662c75b37966a0083c3f9f3874f0a3
Lionel Teo <lionel_teo@imda.gov.sg>
2024-01-08 10:53:15 +0800

    add factscore

--- src/moonshot/data/metrics/bleuscore.py
+++ src/moonshot/data/metrics/bleuscore.py
@@ -4,6 +4,8 @@ from typing import Any
 
 from nltk.translate.bleu_score import sentence_bleu
 
+from moonshot.src.utils.timeit import timeit
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
@@ -14,10 +16,29 @@ class BleuScore:
     """
 
     @staticmethod
-    def get_results(output_response: Any, targets: Any) -> dict:
+    @timeit
+    def get_results(
+        prompts: Any, predicted_results: Any, targets: Any, *args, **kwargs
+    ) -> dict:
+        """
+        Calculate the BLEU score for a list of predicted results and their corresponding target results.
+
+        Args:
+            prompts (Any): The prompts used to generate the predicted results.
+            predicted_results (Any): The list of predicted results.
+            targets (Any): The list of target results.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            dict: A dictionary containing the BLEU score.
+
+        Raises:
+            None
+        """
         bleu_scores = []
-        for idx, (output, target) in enumerate(zip(output_response, targets)):
-            output_split = output.split()
+        for idx, (result, target) in enumerate(zip(predicted_results, targets)):
+            output_split = result.split()
             target_split = target.split()
 
             score = sentence_bleu(output_split, target_split)
