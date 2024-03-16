d72a5a57ba2874a6f832c93d23cd8cda231c64c3
aminmdlahir <aminurrashid_mohamed_lahir@imda.gov.sg>
2024-03-16 16:45:45 +0800

    web api env variables

--- moonshot/integrations/web_api/status_updater/webhook.py
+++ moonshot/integrations/web_api/status_updater/webhook.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import requests
-import os
+from dotenv import dotenv_values
 
 from dependency_injector.wiring import inject
 from ..services.benchmark_test_state import BenchmarkTestState
@@ -9,18 +9,20 @@ from .interface.benchmark_callback_handler import InterfaceBenchmarkCallbackHand
 from ..types.types import CookbookTestRunProgress
 
 class Webhook(InterfaceBenchmarkCallbackHandler):
+    url = "http://localhost:3000/api/v1/benchmarks/status"
+
     @inject
     def __init__(self, benchmark_test_state: BenchmarkTestState) -> None:
         self.benchmark_test_state = benchmark_test_state
+        self.url = dotenv_values().get("MOONSHOT_UI_CALLBACK_URL", "http://localhost:3000/api/v1/benchmarks/status")
 
     def on_executor_update(self, progress_data: CookbookTestRunProgress) -> None:
         logger = logging.getLogger();
         logger.debug(json.dumps(progress_data, indent=2))
         self.benchmark_test_state.update_state(progress_data)
-        url = os.getenv("MOONSHOT_UI_CALLBACK_URL", "http://localhost:3000/api/v1/benchmarks/status")
 
         try:
-            response = requests.post(url, json=progress_data)
+            response = requests.post(self.url, json=progress_data)
             response.raise_for_status()
             logger.log(level=logging.DEBUG, msg=response.json());
             logger.log(level=logging.INFO, msg="Progress data successfully sent to the server.")
