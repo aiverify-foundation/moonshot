ccf016347302eb2429cc9db8ce831a66f9fab850
Norman Chia <normanchia@Normans-MacBook-Pro-2.local>
2024-03-18 16:47:54 +0800

    modify docs and readme

--- moonshot/__main__.py
+++ moonshot/__main__.py
@@ -13,10 +13,10 @@ def main():
         print("Invalid number of argument given.")
         sys.exit(1)
     option = sys.argv[1]
-    if option == "web_api":
+    if option == "web-api":
         web_api.start_app()
     else: 
-        print("Unrecognized arguments. Please use web_api")
+        print("Unrecognized arguments. Please use web-api")
         sys.exit(1)
 
 if __name__ == "__main__":
