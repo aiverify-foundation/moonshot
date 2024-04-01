10112e63e7d634f8703c5c68f8e2415a614ce1d1
Norman Chia <normanchia@192.168.1.12>
2024-04-01 13:25:55 +0800

    add crud routes for endpoint

--- moonshot/integrations/web_api/routes/endpoint.py
+++ moonshot/integrations/web_api/routes/endpoint.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from dependency_injector.wiring import inject, Provide
 
 from ..schemas.endpoint_create_dto import EndpointCreateDTO
@@ -8,25 +8,8 @@ from ..services.endpoint_service import EndpointService
 from ..services.utils.exceptions_handler import ServiceException
 from typing import Optional
 
-router = APIRouter()
 
-@router.get("/v1/llm_endpoints")
-@inject
-def get_all_endpoints(
-    endpoint_service: EndpointService = Depends(Provide[Container.endpoint_service])
-) -> list[Optional[EndpointDataModel]]:
-    """
-    Get all the endpoints from the database
-    """
-    try: 
-        return endpoint_service.get_all_endpoints()
-    except ServiceException as e:
-        if e.error_code == "FileNotFound":
-            raise HTTPException(status_code=404, detail=f"Failed to get endpoint: {e.msg}")
-        elif e.error_code == "ValidationError":
-            raise HTTPException(status_code=400, detail=f"Failed to get endpoint: {e.msg}")
-        else:
-            raise HTTPException(status_code=500, detail=f"Failed to get endpoint: {e.msg}")    
+router = APIRouter()
 
 @router.post("/v1/llm_endpoints")
 @inject
@@ -47,6 +30,84 @@ def add_new_endpoint(
             raise HTTPException(status_code=400, detail=f"Failed to add endpoint: {e.msg}")
         else:
             raise HTTPException(status_code=500, detail=f"Failed to add endpoint: {e.msg}")    
+        
+
+@router.get("/v1/llm_endpoints")
+@inject
+def get_all_endpoints(
+    endpoint_service: EndpointService = Depends(Provide[Container.endpoint_service])
+) -> list[Optional[EndpointDataModel]]:
+    """
+    Get all the endpoints from the database
+    """
+    try:
+        return endpoint_service.get_all_endpoints()
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to get endpoint: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to get endpoint: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to get endpoint: {e.msg}")    
+
+
+@router.get("/v1/llm_endpoints/name")
+@inject
+def get_all_endpoints_name(
+    endpoint_service: EndpointService = Depends(Provide[Container.endpoint_service])
+) -> list[Optional[str]]:
+    """
+    Get all the endpoints from the database
+    """
+    try:
+        return endpoint_service.get_all_endpoints_names()
+    except ServiceException as e:
+        error_status_code = 500
+        if e.error_code == "FileNotFound":
+            error_status_code = 404
+        elif e.error_code == "ValidationError":
+            error_status_code = 400
+        raise HTTPException(status_code=error_status_code, detail=f"Failed to get endpoint: {e.msg}")
+
+
+@router.get("/v1/llm_endpoints/{endpoint_id}")
+@inject
+def get_endpoint(
+    endpoint_id: str,
+    endpoint_service: EndpointService = Depends(Provide[Container.endpoint_service])
+) -> EndpointDataModel | None:
+    """
+    Get an endpoint from the database
+    """
+    try:
+        return endpoint_service.get_endpoint(endpoint_id)
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to get endpoint: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to get endpoint: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to get endpoint: {e.msg}")
+
+
+@router.put("/v1/llm_endpoints/{endpoint_id}")
+@inject
+async def update_endpoint(
+    endpoint_id: str,
+    endpoint_data: EndpointCreateDTO,
+    endpoint_service: EndpointService = Depends(Provide[Container.endpoint_service])
+    ) -> dict[str, str] | tuple[dict[str, str], int]:
+    try:
+        endpoint_service.update_endpoint(endpoint_id, endpoint_data)
+        return {"message": "Endpoint updated successfully"}
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to update endpoint: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to update endpoint: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to update endpoint: {e.msg}")
+
 
 @router.delete("/v1/llm_endpoints/{endpoint_id}")
 @inject
@@ -64,6 +125,7 @@ async def delete_endpoint(
             raise HTTPException(status_code=400, detail=f"Failed to delete endpoint: {e.msg}")
         else:
             raise HTTPException(status_code=500, detail=f"Failed to delete endpoint: {e.msg}")   
+    
     
 @router.get("/v1/connectors")
 @inject
--- moonshot/integrations/web_api/services/endpoint_service.py
+++ moonshot/integrations/web_api/services/endpoint_service.py
@@ -4,17 +4,46 @@ from .base_service import BaseService
 from ..schemas.endpoint_response_model import EndpointDataModel
 from .utils.exceptions_handler import exception_handler
 
+
 class EndpointService(BaseService):
-    @exception_handler
-    def get_all_endpoints(self) -> list[EndpointDataModel | None]:
-        endpoints = moonshot_api.api_get_all_endpoint()
-        return [EndpointDataModel.model_validate(endpoint) for endpoint in endpoints]
 
     @exception_handler
     def add_endpoint(self, endpoint_data: EndpointDataModel) -> None:
         moonshot_api.api_create_endpoint(
-            connector_type=endpoint_data.connector_type,
             name=endpoint_data.name,
+            connector_type=endpoint_data.connector_type,
+            uri=endpoint_data.uri,
+            token=endpoint_data.token,
+            max_calls_per_second=endpoint_data.max_calls_per_second,
+            max_concurrency=endpoint_data.max_concurrency,
+            params=endpoint_data.params
+        )
+    
+
+    @exception_handler
+    def get_all_endpoints(self) -> list[EndpointDataModel | None]:
+        endpoints = moonshot_api.api_get_all_endpoint()
+        return [EndpointDataModel.model_validate(endpoint) for endpoint in endpoints]
+    
+   
+    @exception_handler
+    def get_endpoint(self, endpoint_id: str) -> EndpointDataModel | None:
+        endpoint = moonshot_api.api_read_endpoint(endpoint_id)
+        return EndpointDataModel.model_validate(endpoint)
+
+
+    @exception_handler
+    def get_all_endpoints_names(self) -> list[str]:
+        endpoints_names = moonshot_api.api_get_all_endpoint_name()
+        return endpoints_names
+    
+
+    @exception_handler
+    def update_endpoint(self, endpoint_id: str, endpoint_data: EndpointDataModel) -> None:
+        moonshot_api.api_update_endpoint(
+            ep_id=endpoint_id,
+            name=endpoint_data.name,
+            connector_type=endpoint_data.connector_type,
             uri=endpoint_data.uri,
             token=endpoint_data.token,
             max_calls_per_second=endpoint_data.max_calls_per_second,
@@ -22,11 +51,14 @@ class EndpointService(BaseService):
             params=endpoint_data.params
         )
 
+
     @exception_handler
     def delete_endpoint(self, endpoint_id: str) -> None:
         moonshot_api.api_delete_endpoint(endpoint_id)
+
 
     @exception_handler
     def get_all_connectors(self) -> list[str]:
         connectors = moonshot_api.api_get_all_connector_type()
         return connectors
+    
