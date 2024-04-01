3fbc5d48e738c8da8566faaf75a867429147be7a
Norman Chia <normanchia@192.168.1.12>
2024-04-01 15:09:11 +0800

    add read and delete route for results

--- moonshot/integrations/web_api/services/benchmark_result_service.py
+++ moonshot/integrations/web_api/services/benchmark_result_service.py
@@ -6,14 +6,26 @@ from ..services.base_service import BaseService
 from ..services.utils.exceptions_handler import exception_handler
 
 class BenchmarkResultService(BaseService):
+
     @exception_handler
-    def get_all_results(self, executor_id: str | None = None) -> list[BenchmarkResult] | BenchmarkResult | None:
+    def get_all_results(self) -> list[BenchmarkResult] | BenchmarkResult | None:
         results: list[BenchmarkResult] = moonshot_api.api_get_all_result()
-        if not executor_id:
-            # returning in raw format because tranforming a big list is probably expensive
-            return results
+        return (results)
+    
+
+    @exception_handler
+    def get_all_result_name(self) -> list[str] | None:
+        results = moonshot_api.api_get_all_result_name()
+        return results
+
+
+    @exception_handler
+    def get_result_by_id(self, result_id: str) -> BenchmarkResult | None:
+        result = moonshot_api.api_read_result(result_id)
+        return transform_web_format(result)
+    
+
+    @exception_handler
+    def delete_result(self, result_id: str) -> None:
+        moonshot_api.api_delete_result(result_id)
         
-        for result in results:
-            if result["metadata"]["id"] == executor_id:
-                return transform_web_format(result)
-        return None
