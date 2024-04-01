755e5dea8c1955ee68d28ceef639dca3e7840ee2
Norman Chia <normanchia@192.168.1.12>
2024-04-01 14:35:52 +0800

    add crud routes for recipe

--- moonshot/integrations/web_api/routes/recipe.py
+++ moonshot/integrations/web_api/routes/recipe.py
@@ -10,25 +10,15 @@ from typing import Optional
 
 router = APIRouter()
 
-@router.get("/v1/recipes")
-@inject
-def get_all_recipes(recipe_service: RecipeService = Depends(Provide[Container.recipe_service])):
-    try:
-        return recipe_service.get_all_recipes()
-    except ServiceException as e:
-        if e.error_code == "FileNotFound":
-            raise HTTPException(status_code=404, detail=f"Failed to retrieve recipes: {e.msg}")
-        elif e.error_code == "ValidationError":
-            raise HTTPException(status_code=400, detail=f"Failed to retrieve recipes: {e.msg}")
-        else:
-            raise HTTPException(status_code=500, detail=f"Failed to retrieve recipes: {e.msg}")   
-
 @router.post("/v1/recipes")
 @inject
 def create_recipe(
     recipe_data: RecipeCreateDTO,
     recipe_service: RecipeService = Depends(Provide[Container.recipe_service])
     ):
+    """
+    Add a new recipe to the database
+    """
     try:
         recipe_service.create_recipe(recipe_data)
         return {"message": "Recipe created successfully"}
@@ -39,6 +29,87 @@ def create_recipe(
             raise HTTPException(status_code=400, detail=f"Failed to create recipe: {e.msg}")
         else:
             raise HTTPException(status_code=500, detail=f"Failed to create recipe: {e.msg}")    
+
+
+@router.get("/v1/recipes")
+@inject
+def get_all_recipes(recipe_service: RecipeService = Depends(Provide[Container.recipe_service])
+    ):
+    """
+    Get all the recipes from the database
+    """
+    try:
+        return recipe_service.get_all_recipes()
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to retrieve recipes: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to retrieve recipes: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to retrieve recipes: {e.msg}")   
+
+
+@router.get("/v1/recipes/name")
+@inject
+def get_all_recipes_name(recipe_service: RecipeService = Depends(Provide[Container.recipe_service])
+    ):
+    """
+    Get all the recipes name from the database
+    """
+    try:
+        recipes = recipe_service.get_all_recipes_name()
+        return recipes
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to retrieve cookbooks: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to retrieve cookbooks: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to retrieve cookbooks: {e.msg}")       
+                         
+
+@router.get("/v1/recipes/{recipe_id}")
+@inject 
+def get_recipe_by_id(
+    recipe_id: str,
+    recipe_service: RecipeService = Depends(Provide[Container.recipe_service])
+    ) -> RecipeCreateDTO | None:
+    """
+    Get a recipe from the database
+    """
+    try:
+        recipe = recipe_service.get_recipe_by_id(recipe_id)
+        return recipe
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to retrieve recipe: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to retrieve recipe: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to retrieve recipe: {e.msg}")
+
+
+@router.put("/v1/recipes/{recipe_id}")
+@inject
+async def update_recipe(
+    recipe_data: RecipeCreateDTO,
+    recipe_id: str,
+    recipe_service: RecipeService = Depends(Provide[Container.recipe_service])
+    ) -> dict[str, str] | tuple[dict[str, str], int]:
+    """
+    Update an existing recipe in the database
+    """
+    try:
+        recipe_service.update_recipe(recipe_data, recipe_id)
+        return {"message": "Recipe updated successfully"}
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(status_code=404, detail=f"Failed to update recipe: {e.msg}")
+        elif e.error_code == "ValidationError":
+            raise HTTPException(status_code=400, detail=f"Failed to update recipe: {e.msg}")
+        else:
+            raise HTTPException(status_code=500, detail=f"Failed to update recipe: {e.msg}")
+        
 
 @router.delete("/v1/recipes/{recipe_id}")
 @inject
--- moonshot/integrations/web_api/services/recipe_service.py
+++ moonshot/integrations/web_api/services/recipe_service.py
@@ -3,11 +3,9 @@ from .... import api as moonshot_api
 from ..schemas.recipe_create_dto import RecipeCreateDTO
 from ..services.base_service import BaseService
 from ..services.utils.exceptions_handler import exception_handler
+
+
 class RecipeService(BaseService):
-    @exception_handler
-    def get_all_recipes(self) -> list[dict]:
-        recipes = moonshot_api.api_get_all_recipe()
-        return recipes
 
     @exception_handler
     def create_recipe(self, recipe_data: RecipeCreateDTO) -> None:
@@ -19,6 +17,38 @@ class RecipeService(BaseService):
             prompt_templates=recipe_data.prompt_templates,
             metrics=recipe_data.metrics
         )
+    
+
+    @exception_handler
+    def get_all_recipes(self) -> list[RecipeCreateDTO | None]:
+        recipes = moonshot_api.api_get_all_recipe()
+        return [RecipeCreateDTO.model_validate(recipe) for recipe in recipes]
+    
+
+    @exception_handler
+    def get_all_recipes_name(self) -> list[str]:
+        recipes = moonshot_api.api_get_all_recipe_name()
+        return recipes
+
+
+    @exception_handler
+    def get_recipe_by_id(self, recipe_id: str) -> RecipeCreateDTO | None: 
+        recipe = moonshot_api.api_read_recipe(recipe_id)
+        return RecipeCreateDTO.model_validate(recipe)
+
+
+    @exception_handler
+    def update_recipe(self, recipe_data: RecipeCreateDTO, recipe_id: str) -> None:
+        moonshot_api.api_update_recipe(
+            rec_id=recipe_id,
+            name=recipe_data.name,
+            description=recipe_data.description,
+            tags=recipe_data.tags,
+            datasets=recipe_data.datasets,
+            prompt_templates=recipe_data.prompt_templates,
+            metrics=recipe_data.metrics
+        )
+
 
     @exception_handler
     def delete_recipe(self, recipe_id: str) -> None:
