d4174fe1ede60698de7b4085f65e120c3b6e900c
Norman Chia <normanchia@192.168.1.14>
2024-04-22 20:59:18 +0800

    modify recipe structure for web-api

--- moonshot/integrations/web_api/schemas/recipe_response_dto.py
+++ moonshot/integrations/web_api/schemas/recipe_response_dto.py
@@ -8,5 +8,4 @@ class RecipeResponseDTO(BaseModel):
     datasets: list[str]
     prompt_templates: list[str]
     metrics: list[str]
-    type: str
-    attack_strategies: list[dict]
+    attack_modules: list[str]
