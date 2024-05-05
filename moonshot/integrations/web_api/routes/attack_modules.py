ef6258b645f6f35b50665f879d27490b41c5d8c7
Norman Chia <normanchia@192.168.1.10>
2024-05-06 04:59:59 +0800

    update attack module endpoint to call from api

--- moonshot/integrations/web_api/routes/attack_modules.py
+++ moonshot/integrations/web_api/routes/attack_modules.py
@@ -24,24 +24,3 @@ def get_all_attack_module(am_service: AttackModuleService = Depends(Provide[Cont
             raise HTTPException(status_code=400, detail=f"Failed to retrieve attack modules: {e.msg}")
         else:
             raise HTTPException(status_code=500, detail=f"Failed to retrieve attack modules: {e.msg}")   
-
-
-@router.get("/api/v1/attack-modules/{am_id}")
-@inject 
-def get_attack_module_by_id(
-    am_id: str,
-    am_service: AttackModuleService = Depends(Provide[Container.am_service])
-    ) -> str:
-    """
-    Get a attack modules from the database
-    """
-    try:
-        am = am_service.get_attack_module_by_id(am_id)
-        return am
-    except ServiceException as e:
-        if e.error_code == "FileNotFound":
-            raise HTTPException(status_code=404, detail=f"Failed to retrieve attack modules: {e.msg}")
-        elif e.error_code == "ValidationError":
-            raise HTTPException(status_code=400, detail=f"Failed to retrieve attack modules: {e.msg}")
-        else:
-            raise HTTPException(status_code=500, detail=f"Failed to retrieve attack modules: {e.msg}")
