78a07fedf2ab2fdd2b313a451a12d95cb19cdf28
Norman Chia <normanchia@192.168.1.10>
2024-05-06 05:00:14 +0800

    update endpoint schema to be more flexible in param

--- moonshot/integrations/web_api/schemas/endpoint_response_model.py
+++ moonshot/integrations/web_api/schemas/endpoint_response_model.py
@@ -10,4 +10,4 @@ class EndpointDataModel(BaseModel):
     token: str
     max_calls_per_second: int
     max_concurrency: int
-    params: Optional[dict[str, Union[str, int]]] = None
+    params: Optional[dict] = None
