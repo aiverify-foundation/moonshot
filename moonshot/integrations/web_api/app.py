6eca8c9b9ed8277cee196bef615993e5b0a4fe2f
Norman Chia <normanchia@Normans-MacBook-Pro-2.local>
2024-05-07 13:05:47 +0800

    remove report analysis

--- moonshot/integrations/web_api/app.py
+++ moonshot/integrations/web_api/app.py
@@ -2,32 +2,34 @@ import asyncio
 import logging
 from contextlib import asynccontextmanager
 from typing import Awaitable, Callable
+
+from dependency_injector.wiring import providers
 from fastapi import FastAPI, Request, Response
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from dependency_injector.wiring import providers
+
 from .container import Container
-from .routes.redteam import router as red_team_router
 from .routes import (
     attack_modules,
-    benchmark, 
-    cookbook, 
+    benchmark,
+    benchmark_result,
+    cookbook,
+    dataset,
     endpoint,
-    recipe, 
-    benchmark_result, 
-    prompt_template, 
-    metric, 
+    metric,
+    prompt_template,
+    recipe,
     runner,
-    report_analysis,
-    dataset
-    )
+)
+from .routes.redteam import router as red_team_router
 
 logger = logging.getLogger(__name__)
 
 
 class CustomFastAPI(FastAPI):
     container: Container
+
 
 async def monitor_tasks(loop: asyncio.AbstractEventLoop):
     while True:
@@ -36,30 +38,35 @@ async def monitor_tasks(loop: asyncio.AbstractEventLoop):
             logger.debug(task)
         await asyncio.sleep(1)
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     loop = asyncio.get_running_loop()
     loop.create_task(monitor_tasks(loop))
     yield
 
-async def log_request_origin(request: Request, call_next: Callable[[Request], Awaitable[Response]]):
-    origin = request.headers.get('origin')
+
+async def log_request_origin(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+):
+    origin = request.headers.get("origin")
     logger.info(f"Request origin: {origin}")
     response = await call_next(request)
     return response
 
+
 def create_app(cfg: providers.Configuration) -> CustomFastAPI:
     if cfg.asyncio.monitor_task():
-        logger.warn(f"Monitoring tasks in uvicorn's asyncio event loop")
+        logger.warn("Monitoring tasks in uvicorn's asyncio event loop")
 
     app_kwargs = {}
     if cfg.asyncio.monitor_task():
         app_kwargs["lifespan"] = lifespan
-    
+
     app: CustomFastAPI = CustomFastAPI(**app_kwargs)
 
     if cfg.cors.enabled():
-        logger.info(f"CORS is enabled")
+        logger.info("CORS is enabled")
         allowed_origins_raw: str = cfg.cors.allowed_origins()
         allowed_origins = allowed_origins_raw.split(",") if allowed_origins_raw else []
         app.add_middleware(
@@ -70,11 +77,11 @@ def create_app(cfg: providers.Configuration) -> CustomFastAPI:
             allow_headers=["*"],
         )
     else:
-        logger.warn(f"CORS is disabled")
+        logger.warn("CORS is disabled")
 
     if cfg.app_environment().upper() in ["DEV", "DEVELOPMENT", "LOCAL"]:
         app.middleware("http")(log_request_origin)
-    
+
     app.include_router(red_team_router)
     app.include_router(prompt_template.router)
     app.include_router(benchmark.router)
@@ -84,18 +91,18 @@ def create_app(cfg: providers.Configuration) -> CustomFastAPI:
     app.include_router(benchmark_result.router)
     app.include_router(metric.router)
     app.include_router(runner.router)
-    app.include_router(report_analysis.router)
     app.include_router(dataset.router)
     app.include_router(attack_modules.router)
-    
 
     @app.exception_handler(RequestValidationError)
-    async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    async def validation_exception_handler(
+        request: Request, exc: RequestValidationError
+    ) -> JSONResponse:
         modified_errors: list[str] = []
         for error in exc.errors():
             # Remove the 'url' key from the error detail if it exists
-            if 'url' in error:
-                del error['url']
+            if "url" in error:
+                del error["url"]
             modified_errors.append(error)
 
         logger.error(f"Validation error for request {request.url}: {exc.errors()}")
@@ -105,4 +112,3 @@ def create_app(cfg: providers.Configuration) -> CustomFastAPI:
         )
 
     return app
-
