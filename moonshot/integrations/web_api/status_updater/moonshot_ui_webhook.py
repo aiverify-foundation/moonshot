c1bfeb93fb130e5d75fd36b0614f41fbd3deab9c
Norman Chia <normanchia@Normans-MacBook-Pro-2.local>
2024-05-07 12:46:41 +0800

    fix linting for status updater  folder

--- moonshot/integrations/web_api/status_updater/moonshot_ui_webhook.py
+++ moonshot/integrations/web_api/status_updater/moonshot_ui_webhook.py
@@ -1,13 +1,14 @@
 import json
 import logging
+
 import requests
+from dependency_injector.wiring import inject
 from dotenv import dotenv_values
 
-from dependency_injector.wiring import inject
-
 from ..services.benchmark_test_state import BenchmarkTestState
-from .interface.benchmark_progress_callback import InterfaceBenchmarkProgressCallback
 from ..types.types import TestRunProgress
+from .interface.benchmark_progress_callback import InterfaceBenchmarkProgressCallback
+
 
 class MoonshotUIWebhook(InterfaceBenchmarkProgressCallback):
     """
@@ -18,19 +19,24 @@ class MoonshotUIWebhook(InterfaceBenchmarkProgressCallback):
     @inject
     def __init__(self, benchmark_test_state: BenchmarkTestState) -> None:
         self.benchmark_test_state = benchmark_test_state
-        self.url = str(dotenv_values().get("MOONSHOT_UI_CALLBACK_URL", "http://localhost:3000/api/v1/benchmarks/status"))
+        self.url = str(
+            dotenv_values().get(
+                "MOONSHOT_UI_CALLBACK_URL",
+                "http://localhost:3000/api/v1/benchmarks/status",
+            )
+        )
 
     def on_progress_update(self, progress_data: TestRunProgress) -> None:
-        logger = logging.getLogger();
+        logger = logging.getLogger()
         logger.debug(json.dumps(progress_data, indent=2))
         self.benchmark_test_state.update_progress_status(progress_data)
 
         try:
             response = requests.post(self.url, json=progress_data)
             response.raise_for_status()
-            logger.log(level=logging.DEBUG, msg=response.json());
-            logger.log(level=logging.INFO, msg="Progress data successfully sent to the server.")
+            logger.log(level=logging.DEBUG, msg=response.json())
+            logger.log(
+                level=logging.INFO, msg="Progress data successfully sent to the server."
+            )
         except requests.RequestException as e:
             logger.critical(msg=f"Failed to send progress data: {e}")
-
-
