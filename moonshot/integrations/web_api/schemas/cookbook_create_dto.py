ceb66a263910d58c3b0d9b693567929507f34110
Norman Chia <normanchia@Normans-MacBook-Pro-2.local>
2024-05-17 16:22:51 +0800

    update cookbook dto

--- moonshot/integrations/web_api/schemas/cookbook_create_dto.py
+++ moonshot/integrations/web_api/schemas/cookbook_create_dto.py
@@ -11,6 +11,6 @@ class CookbookCreateDTO(CookbookPydanticModel):
 
 class CookbookUpdateDTO(CookbookPydanticModel):
     id: Optional[str] = None
-    name: Optional[str] = None
-    description: Optional[str] = None
-    recipes: Optional[list[str]] = None
+    name: Optional[str] = None = Field(min_length=1)
+    description: Optional[str] = None = Field(min_length=3, max_length=1000)
+    recipes: Optional[list[str]] = None = Field(min_length=1)
