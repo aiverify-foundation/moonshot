1495a367a9d759038b93af4abb4cb687a141e096
Norman Chia <normanchia@Normans-MacBook-Pro-2.local>
2024-07-02 11:42:36 +0800

    add bookmark integration for web-api

---
+++ moonshot/integrations/web_api/routes/bookmark.py
@@ -0,0 +1,135 @@
+from typing import Optional
+
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends, HTTPException, Query
+
+from ..container import Container
+from ..schemas.bookmark_create_dto import BookmarkCreateDTO, BookmarkPydanticModel
+from ..services.bookmark_service import BookmarkService
+from ..services.utils.exceptions_handler import ServiceException
+
+router = APIRouter(tags=["Bookmark"])
+
+
+@router.post(
+    "/api/v1/bookmarks", response_description="Bookmark data added to the database"
+)
+@inject
+def insert_bookmark(
+    bookmark_data: BookmarkCreateDTO,
+    bookmark_service: BookmarkService = Depends(Provide[Container.bookmark_service]),
+) -> dict:
+    """
+    Insert a new bookmark into the database.
+
+    Args:
+        bookmark_data: The data of the bookmark to be added.
+        bookmark_service: The service responsible for bookmark operations.
+
+    Returns:
+        A dictionary with a message indicating successful insertion.
+
+    Raises:
+        HTTPException: An error occurred while inserting the bookmark.
+    """
+    try:
+        bookmark_service.insert_bookmark(bookmark_data)
+        return {"message": "Bookmark added successfully"}
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(
+                status_code=404, detail=f"Failed to insert bookmark: {e.msg}"
+            )
+        elif e.error_code == "ValidationError":
+            raise HTTPException(
+                status_code=400, detail=f"Failed to insert bookmark: {e.msg}"
+            )
+        else:
+            raise HTTPException(
+                status_code=500, detail=f"Failed to insert bookmark: {e.msg}"
+            )
+
+
+@router.get(
+    "/api/v1/bookmarks",
+    response_description="List of all bookmarks or a specific bookmark by ID",
+)
+@inject
+def get_all_bookmarks(
+    id: Optional[int] = Query(None, description="ID of the bookmark to query"),
+    bookmark_service: BookmarkService = Depends(Provide[Container.bookmark_service]),
+) -> list[BookmarkPydanticModel]:
+    """
+    Retrieve all bookmarks or a specific bookmark by ID from the database.
+
+    Args:
+        id: The ID of the bookmark to retrieve. If None, all bookmarks are retrieved.
+        bookmark_service: The service responsible for bookmark operations.
+
+    Returns:
+        A list of bookmarks or a single bookmark if an ID is provided.
+
+    Raises:
+        HTTPException: An error occurred while retrieving bookmarks.
+    """
+    try:
+        return bookmark_service.get_all_bookmarks(
+            id=id,
+        )
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(
+                status_code=404, detail=f"Failed to retrieve bookmarks: {e.msg}"
+            )
+        elif e.error_code == "ValidationError":
+            raise HTTPException(
+                status_code=400, detail=f"Failed to retrieve bookmarks: {e.msg}"
+            )
+        else:
+            raise HTTPException(
+                status_code=500, detail=f"Failed to retrieve bookmarks: {e.msg}"
+            )
+
+
+@router.delete(
+    "/api/v1/bookmarks", response_description="Bookmark data deleted from the database"
+)
+@inject
+def delete_bookmark(
+    all: bool = Query(False, description="Flag to delete all bookmarks"),
+    id: Optional[int] = Query(None, description="ID of the bookmark to delete"),
+    bookmark_service: BookmarkService = Depends(Provide[Container.bookmark_service]),
+) -> dict:
+    """
+    Delete a specific bookmark or all bookmarks from the database.
+
+    Args:
+        all: A flag indicating whether to delete all bookmarks.
+        id: The ID of the bookmark to delete.
+        bookmark_service: The service responsible for bookmark operations.
+
+    Raises:
+        HTTPException: An error occurred while deleting the bookmark(s).
+    """
+    try:
+        if all:
+            return bookmark_service.delete_bookmarks(all=True)
+        elif id is not None:
+            return bookmark_service.delete_bookmarks(all=False, id=id)
+        else:
+            raise HTTPException(
+                status_code=400, detail="Must specify 'all' or 'id' parameter"
+            )
+    except ServiceException as e:
+        if e.error_code == "FileNotFound":
+            raise HTTPException(
+                status_code=404, detail=f"Failed to delete bookmark: {e.msg}"
+            )
+        elif e.error_code == "ValidationError":
+            raise HTTPException(
+                status_code=400, detail=f"Failed to delete bookmark: {e.msg}"
+            )
+        else:
+            raise HTTPException(
+                status_code=500, detail=f"Failed to delete bookmark: {e.msg}"
+            )
---
+++ moonshot/integrations/web_api/services/bookmark_service.py
@@ -0,0 +1,88 @@
+from .... import api as moonshot_api
+from ..schemas.bookmark_create_dto import BookmarkCreateDTO, BookmarkPydanticModel
+from ..services.base_service import BaseService
+from ..services.utils.exceptions_handler import exception_handler
+
+
+class BookmarkService(BaseService):
+    @exception_handler
+    def insert_bookmark(self, bookmark_data: BookmarkCreateDTO) -> dict:
+        """
+        Inserts a new bookmark into the system.
+
+        Args:
+            bookmark_data (BookmarkCreateDTO): The data transfer object containing bookmark details.
+        """
+        result = moonshot_api.api_insert_bookmark(
+            name=bookmark_data.name,
+            prompt=bookmark_data.prompt,
+            response=bookmark_data.response,
+            context_strategy=bookmark_data.context_strategy,
+            prompt_template=bookmark_data.prompt_template,
+            attack_module=bookmark_data.attack_module,
+        )
+        if not result["success"]:
+            raise Exception(result["message"], "insert_bookmark", "InsertBookmarkError")
+        return result
+
+    @exception_handler
+    def get_all_bookmarks(self, id: int | None = None) -> list[BookmarkPydanticModel]:
+        """
+        Retrieves all bookmarks or a specific bookmark by its ID.
+
+        Args:
+            id (int | None, optional): The ID of the bookmark to retrieve. If None, all bookmarks are retrieved.
+
+        Returns:
+            list[BookmarkPydanticModel]: A list of bookmark models.
+        """
+        retn_bookmark: list[BookmarkPydanticModel] = []
+
+        if id:
+            bookmarks = [moonshot_api.api_get_bookmark_by_id(id)]
+        else:
+            bookmarks = moonshot_api.api_get_all_bookmarks()
+
+        for bookmark in bookmarks:
+            retn_bookmark.append(BookmarkPydanticModel(**bookmark))
+        return retn_bookmark
+
+    @exception_handler
+    def delete_bookmarks(self, all: bool = False, id: int | None = None) -> dict:
+        """
+        Deletes bookmarks from the system either by ID or all bookmarks if specified.
+
+        Args:
+            all (bool, optional): If True, all bookmarks will be deleted. Defaults to False.
+            id (int | None, optional): The ID of the bookmark to delete.
+        """
+        if all:
+            result = moonshot_api.api_delete_all_bookmark()
+        elif id is not None:
+            result = moonshot_api.api_delete_bookmark(id)
+        else:
+            raise ValueError("Either 'all' must be True or 'id' must be provided.")
+
+        if not result["success"]:
+            raise Exception(
+                result["message"], "delete_bookmarks", "DeleteBookmarkError"
+            )
+
+        print(result)
+        return result
+
+    @exception_handler
+    def export_bookmarks(
+        self, write_file: bool = False, export_file_name: str = "bookmarks"
+    ) -> list[dict]:
+        """
+        Exports bookmarks to a file or returns them as a list of dictionaries.
+
+        Args:
+            write_file (bool, optional): If True, bookmarks will be written to a file. Defaults to False.
+            export_file_name (str, optional): The name of the file to write bookmarks to. Defaults to "bookmarks".
+
+        Returns:
+            list[dict]: A list of bookmarks as dictionaries.
+        """
+        return moonshot_api.api_export_bookmarks(write_file, export_file_name)
