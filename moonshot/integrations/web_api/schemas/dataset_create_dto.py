792cdd50c6238a782c75e3dc0ff9f8b1025c59b4
Norman Chia <normanchia@192.168.1.16>
2024-08-12 17:34:18 +0800

    update web-api dataset creation

--- moonshot/integrations/web_api/schemas/dataset_create_dto.py
+++ moonshot/integrations/web_api/schemas/dataset_create_dto.py
@@ -8,11 +8,21 @@ from moonshot.src.datasets.dataset_arguments import (
 )
 
 
-class DatasetCreateDTO(DatasetPydanticModel):
-    id: Optional[str] = None
-    examples: Iterator[dict] = None
+class CSV_Dataset_DTO(DatasetPydanticModel):
+    id: Optional[str] = None  # Not a required from user
+    examples: Optional[Iterator[dict]] = None  # Not a required from user
     name: str = Field(..., min_length=1)
     description: str = Field(default="", min_length=1)
     license: Optional[str] = ""
     reference: Optional[str] = ""
-    params: dict
+    csv_file_path: str = Field(..., min_length=1)
+
+
+class HF_Dataset_DTO(DatasetPydanticModel):
+    id: Optional[str] = None  # Not a required from user
+    examples: Optional[Iterator[dict]] = None  # Not a required from user
+    name: str = Field(..., min_length=1)
+    description: str = Field(default="", min_length=1)
+    license: Optional[str] = ""
+    reference: Optional[str] = ""
+    params: dict = Field(..., min_length=1)
