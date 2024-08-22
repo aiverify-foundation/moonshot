f08fcab505238655fb49459f5f1b680159ec7c7d
Lionel Teo <93119265+imda-lionelteo@users.noreply.github.com>
2024-08-22 13:23:04 +0800

    add bookmark unit tests

---
+++ moonshot/src/messages_constants.py
@@ -0,0 +1,38 @@
+# ------------------------------------------------------------------------------
+# BOOKMARK - add_bookmark
+# ------------------------------------------------------------------------------
+BOOKMARK_ADD_BOOKMARK_SUCCESS = "[Bookmark] Bookmark added successfully."
+BOOKMARK_ADD_BOOKMARK_PREFIX = "[Bookmark] Failed to add bookmark record:"
+BOOKMARK_ADD_BOOKMARK_VALIDATION_ERROR = "Error inserting record into database."
+
+# ------------------------------------------------------------------------------
+# BOOKMARK - get_bookmark
+# ------------------------------------------------------------------------------
+BOOKMARK_GET_BOOKMARK_PREFIX = "[Bookmark] No record found for bookmark name:"
+BOOKMARK_GET_BOOKMARK_PREFIX_1 = "[Bookmark] Invalid bookmark name:"
+
+# ------------------------------------------------------------------------------
+# BOOKMARK - delete_bookmark
+# ------------------------------------------------------------------------------
+BOOKMARK_DELETE_BOOKMARK_SUCCESS = "[Bookmark] Bookmark record deleted."
+BOOKMARK_DELETE_BOOKMARK_PREFIX = "[Bookmark] Failed to delete bookmark record:"
+BOOKMARK_DELETE_BOOKMARK_PREFIX_1 = "[Bookmark] Invalid bookmark name:"
+
+# ------------------------------------------------------------------------------
+# BOOKMARK - delete_all_bookmark
+# ------------------------------------------------------------------------------
+BOOKMARK_DELETE_ALL_BOOKMARK_SUCCESS = "[Bookmark] All bookmark records deleted."
+BOOKMARK_DELETE_ALL_BOOKMARK_PREFIX = (
+    "[Bookmark] Failed to delete all bookmark records:"
+)
+
+# ------------------------------------------------------------------------------
+# BOOKMARK - export_bookmarks
+# ------------------------------------------------------------------------------
+BOOKMARK_EXPORT_BOOKMARK_PREFIX = "[Bookmark] Failed to export bookmarks:"
+BOOKMARK_EXPORT_BOOKMARK_VALIDATION_ERROR = "Export filename must be a non-empty string"
+
+# ------------------------------------------------------------------------------
+# BOOKMARK ARGUMENTS - from_tuple_to_dict
+# ------------------------------------------------------------------------------
+BOOKMARK_ARGUMENTS_FROM_TUPLE_TO_DICT_VALIDATION_ERROR = "[BookmarkArguments] Failed to convert to dictionary because of the insufficient number of values"  # noqa: E501
