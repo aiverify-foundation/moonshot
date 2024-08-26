058f706fb217b0b3e70bb8301ad813939e715dc5
Lionel Teo <93119265+imda-lionelteo@users.noreply.github.com>
2024-08-26 22:59:35 +0800

    use docker compose, add pvc

--- examples/deployment/run_installation_script.sh
+++ examples/deployment/run_installation_script.sh
@@ -1,6 +1,9 @@
 # install moonshot
 pip3 install --no-cache-dir "aiverify-moonshot[all]"
 
+# install in /app/data
+cd /app/data
+
 # install moonshot-data and moonshot-ui
 python -m moonshot -i moonshot-data -i moonshot-ui
 
