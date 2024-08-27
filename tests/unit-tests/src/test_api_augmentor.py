43726047f8f2b65fbde61d0eb805b52d3f7616ed
Norman Chia <normanchia@192.168.1.14>
2024-08-27 16:35:59 +0800

    update unit test for augmentator

--- tests/unit-tests/src/test_api_augmentor.py
+++ tests/unit-tests/src/test_api_augmentor.py
@@ -17,7 +17,8 @@ class TestCollectionApiAugmentor:
         api_set_environment_variables(
             {
                 "ATTACK_MODULES": "tests/unit-tests/src/data/attack-modules/",
-                "RECIPE": "tests/unit-tests/src/data/bookmarks/",
+                "RECIPES": "tests/unit-tests/src/data/recipes/",
+                "METRICS": "tests/unit-tests/src/data/metrics/",
                 "DATASETS": "tests/unit-tests/src/data/datasets/",
                 "IO_MODULES": "tests/unit-tests/src/data/io-modules/",
             }
@@ -33,13 +34,18 @@ class TestCollectionApiAugmentor:
             "tests/unit-tests/src/data/recipes/test-recipe.json",
         )
 
+        shutil.copyfile(
+            "tests/unit-tests/common/samples/exactstrmatch.py",
+            "tests/unit-tests/src/data/metrics/exactstrmatch.py",
+        )
         yield
 
         run_data_files = [
             "tests/unit-tests/src/data/datasets/small-test-dataset.json",
             "tests/unit-tests/src/data/datasets/small-test-dataset-attack-module.json",
             "tests/unit-tests/src/data/recipes/test-recipe.json",
-
+            "tests/unit-tests/src/data/recipes/test-recipe-attack-module.json",
+            "tests/unit-tests/src/data/metrics/exactstrmatch.py",
         ]
         for run_data_file in run_data_files:
             if os.path.exists(run_data_file):
@@ -89,11 +95,38 @@ class TestCollectionApiAugmentor:
             result = api_augment_dataset(dataset_id, attack_module_id)
             assert result == expected_response
 
+
+    # ------------------------------------------------------------------------------
+    # Test api_augment_recipe functionality
+    # ------------------------------------------------------------------------------
+    @pytest.mark.parametrize(
+        "recipe_id, attack_module_id, expected_response",
+        [   
+            # Success Scenario
+            ("test-recipe", "attack_module", "test-recipe-attack-module"),
+            # Empty Recipe
+            ("", "attack_module", ValueError),
+            # Empty Attack Module
+            ("test-recipe", "", ValueError),
+            # Empty Both
+            ("", "", ValueError),
+            # None Recipe
+            (None, "attack_module", ValueError),
+            # None Attack Module
+            ("small-test-dataset", None, ValueError),
+            # Both None
+            (None, None, ValueError),
+            # Invalid Recipe
+            ("missing-recipe", "attack_module", RuntimeError),
+            # Invalid Attack Module TODO scenario only valid after merging with attack module
+            # ("small-test-dataset", "missing-attack_module", RuntimeError),
+        ]
+    )
     def test_api_augment_recipe(self, recipe_id, attack_module_id, expected_response):
         if expected_response is RuntimeError:
             expected_messages = [
-                "No datasets found with ID:",
-                "Dataset ID is empty",
+                "No recipes found with ID:",
+                "Recipe ID is empty",
             ]
             with pytest.raises(RuntimeError) as excinfo:
                 api_augment_recipe(recipe_id, attack_module_id)
