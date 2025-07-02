2b0e32d8c75fa3fedb350ca025a519a0418d09e4
Kayden Yip <kayden_yip@imda.gov.sg>
2025-07-02 16:05:38 +0800

    Enhancement to check runner name and endpoints are not empty when creating runner.

--- moonshot/src/api/api_runner.py
+++ moonshot/src/api/api_runner.py
@@ -38,10 +38,13 @@ def api_create_runner(
     # This is because during creating:
     # 1. the id is slugify from the name and stored as id.
     # Validate arguments
-    if not name or not name.strip():
+    if not name.strip():
         raise ValueError("Runner name must not be empty.")
-    if not endpoints or not isinstance(endpoints, list) or not all(isinstance(e, str) for e in endpoints):
+
+    if not endpoints or not isinstance(endpoints, list) \
+            or not all([e.strip() for e in endpoints]):
         raise ValueError("Endpoints must be a non-empty list of strings.")
+
     runner_args = RunnerArguments(
         id="",
         name=name,
